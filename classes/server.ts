88e36c238d188fd63ca9d48abafd0bf70bb44f48
Angel Montedoro <amontedoro.leoncito@gmail.com>
2021-08-25 11:41:52 -0500

    Cambios sockets

--- classes/server.ts
+++ classes/server.ts
@@ -1,14 +1,34 @@
 
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server{
     public app: express.Application;
     public port:number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
     constructor(){
         this.app = express();
         this.port = SERVER_PORT; 
+
+        this.httpServer = new http.Server(this.app)
+        this.io = new socketIO.Server( this.httpServer, { cors: { origin: true, credentials: true } } );
+    }
+
+
+    private escucharSockets() {
+
+        console.log("escuchar conexiones - sockets");
+
+        this.io.on('connection', cliente =>{
+        
+            console.log("Cliente conectado"); 
+        
+        })
     }
 
     //start(callback: any){
@@ -16,6 +36,7 @@ export default class Server{
     // }
 
     start(callback : Function){
-        this.app.listen(this.port, callback());
+        this.httpServer.listen(this.port, callback());
+        //this.app.listen(this.port, callback());
     }
 }
